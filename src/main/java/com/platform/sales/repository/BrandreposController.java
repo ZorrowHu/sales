3bf1879ec07a361a6e5353e6f26fac3f2708effc
ZorrowHu <354243111@qq.com>
2018-07-24 17:30:33 +0800

    BrandreposRepository

--- src/main/java/com/platform/sales/repository/BrandreposController.java
+++
@@ -1,7 +0,0 @@
-package com.platform.sales.repository;
-
-import com.platform.sales.entity.Brandrepos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrandreposController extends JpaRepository<Brandrepos, Integer> {
-}
---
+++ src/main/java/com/platform/sales/repository/BrandreposRepository.java
@@ -0,0 +1,7 @@
+package com.platform.sales.repository;
+
+import com.platform.sales.entity.Brand_repos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandreposRepository extends JpaRepository<Brand_repos, Integer> {
+}
