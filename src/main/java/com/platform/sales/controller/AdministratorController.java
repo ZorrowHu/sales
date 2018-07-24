81b951dd76de507911e79becf456be2dd6f568a0
ChingQ <1178097316@qq.com>
2018-07-24 17:49:29 +0800

    秦 管理员页面

---
+++ src/main/java/com/platform/sales/controller/AdministratorController.java
@@ -0,0 +1,16 @@
+package com.platform.sales.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("administrator")
+public class AdministratorController {
+
+    @GetMapping("index")
+    public String getAllOrders(){
+        return "administrator/index";
+    }
+
+}
