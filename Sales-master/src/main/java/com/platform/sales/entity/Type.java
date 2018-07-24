4784d697726c8daafe3cdf0ac243123483bfa3b9
Lawrence <979676833@qq.com>
2018-07-24 11:04:09 +0800

    Add files via upload

---
+++ Sales-master/src/main/java/com/platform/sales/entity/Type.java
@@ -0,0 +1,12 @@
+package com.platform.sales.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+
+@Entity
+public class Type {
+
+        @Id
+        @GeneratedValue
+        public Integer type_id;       //类型id
+}
