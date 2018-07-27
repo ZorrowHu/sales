38bf26eeeda2d387662f8af7ad34fdef896d104b
Lawrence <979676833@qq.com>
2018-07-27 17:07:02 +0800

    deleted:    src/main/java/com/platform/sales/surface/StoreorderService.java deleted:    src/main/java/com/platform/sales/surface/StoreorderServiceImpl.java

--- src/main/java/com/platform/sales/surface/StoreorderService.java
+++
@@ -1,8 +0,0 @@
-package com.platform.sales.surface;
-
-import java.util.List;
-
-public interface StoreorderService {
-    //根据借卖方的ID查找所有订单
-    public List<StoreOrder> findAllBySeller_UserId(Integer id);
-}
