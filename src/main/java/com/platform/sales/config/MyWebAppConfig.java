e26ba36a3bf7cee4fd4403e7d47db8785c0fce6f
ZorrowHu <354243111@qq.com>
2018-07-27 11:01:23 +0800

    modified:   src/main/java/com/platform/sales/config/MyWebAppConfig.java 	modified:   src/main/java/com/platform/sales/controller/BrandreposController.java 	deleted:    src/main/resources/static/goodsimg/1532595733175thumb5.jpg 	modified:   src/main/resources/templates/brand/mainframe.html

--- src/main/java/com/platform/sales/config/MyWebAppConfig.java
+++ src/main/java/com/platform/sales/config/MyWebAppConfig.java
@@ -1,4 +1,18 @@
 package com.platform.sales.config;
-
-public class MyWebAppConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/**
+ * Created by 18274 on 2017/8/9.
+ */
+@Configuration
+public class MyWebAppConfig extends WebMvcConfigurerAdapter{
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                //registry.addResourceHandler("/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/goodsimg/");
+                registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
+                registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+                super.addResourceHandlers(registry);
+        }
 }
